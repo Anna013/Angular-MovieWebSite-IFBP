edd5d1804c48959fb1efe81e9164fa26f805aabd
Anna013 <anjovanic13@gmail.com>
2022-04-04 12:29:36 +0200

    log i reg

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,11 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { Guard } from './guard/guard';
-import { TokenInterceptor } from './services/token.interceptor';
 import { UserLoginComponent } from './user/user-login/user-login.component';
 import { UserRegisterComponent } from './user/user-register/user-register.component';
-import { AdminGuard } from './guard/adminGuard';
-import { MovieService } from './services/movie.service';
 import {NgxPaginationModule} from 'ngx-pagination';
-import { MovieListComponent } from './movie/movie-list/movie-list.component';
-import { AddMovieComponent } from './movie/add-movie/add-movie.component';
-import { MovieItemComponent } from './movie/movie-item/movie-item.component';
 
 
 
@@ -22,11 +14,7 @@ import { MovieItemComponent } from './movie/movie-item/movie-item.component';
   declarations: [
     AppComponent,
     UserLoginComponent,
-    UserRegisterComponent,
-    MovieListComponent,
-    AddMovieComponent,
-    MovieItemComponent,
-    
+    UserRegisterComponent
 
   ],
   imports: [
@@ -38,7 +26,7 @@ import { MovieItemComponent } from './movie/movie-item/movie-item.component';
    
    
   ],
-  providers: [MovieService, {provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi:true}, Guard, AdminGuard],
+  providers: [ ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
